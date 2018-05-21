1ef22a7752fe4a8df952344832bd76edc4dfda7d
Greentea1120 <741024920@qq.com>
2018-05-22 01:20:35 +0800

    quotation

--- src/page/Quotation/Quotation.vue
+++ src/page/Quotation/Quotation.vue
@@ -9,6 +9,25 @@
         <li>涨跌幅</li>
       </ul>
     </div>
+    <Scroll class="scroll">
+      <div class="quotation-list">
+        <ul>
+          <li class="quotation-item" v-for="(item, index) in list" :key="index">
+            <div class="quotation-content name-wrapper">
+              <span class="name">{{item.name}}</span>
+              <span class="name-en">{{item.en}}</span>
+            </div>
+            <div class="quotation-content desc-wrapper">
+              <span class="price">¥: {{item.price}}</span>
+              <span class="number">数量: {{item.number}}</span>
+            </div>
+            <div class="quotation-content echarts">
+
+            </div>
+          </li>
+        </ul>
+      </div>
+    </Scroll>
   </div>
 </template>
 
@@ -21,7 +40,96 @@ import List from 'components/List/List'
 export default {
   data () {
     return {
-
+      list: [{
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      },
+      {
+        name: '绝地求生',
+        en: 'JUEDIQIUSHENG',
+        price: 122,
+        number: 10
+      }]
     }
   },
   components: {
@@ -39,13 +147,49 @@ export default {
   .quotation
     fixed-all()
     .quotation-title
+      position relative
+      z-index 2
       padding-top 45px
       height 40px
       line-height 40px
+      box-shadow: 0 10px 20px #272f4b
       ul
         display flex
         justify-content space-around
         li
           flex 1
           text-align center
+    .scroll
+      fixed-all()
+      top 135px
+      .quotation-list
+        ul
+          padding 0 13px
+          .quotation-item
+            height 72px
+            border-radius 16px
+            background-image:linear-gradient(90deg, #4c5779, #2e3859)
+            display flex
+            margin-bottom 10px
+            .quotation-content
+              flex 1
+              box-sizing border-box
+              &.name-wrapper
+                display flex
+                flex-direction column
+                justify-content space-around
+                padding 15px 0 15px 15px
+                .name
+                  font-size $font-size-medium
+                  color #fff
+                .number
+                  color $color-text-desc
+              &.desc-wrapper
+                display flex
+                flex-direction column
+                justify-content space-around
+                text-align center
+                padding 15px 0
+                .price
+                  color $color-text-money
 </style>
