1bc0e1677a22b0c6664d4b75f3a125de0f214fdf
Greentea1120 <741024920@qq.com>
2018-06-17 04:22:12 +0800

    init websocket

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import { mapActions } from 'vuex'
 export default {
   mounted() {
     this.setUser()
+    this.initWebpack()
   },
   data () {
     return {
@@ -21,7 +22,34 @@ export default {
     Layout
   },
   methods: {
-    ...mapActions(['setUser'])
+    ...mapActions(['setUser']),
+    initWebpack() {
+      // 初始化websocket
+      const wsuri = 'ws://47.104.254.248:8080/api/message/message-count'
+      this.websock = new WebSocket(wsuri)
+      // 这里面的this都指向vue
+      this.websock.onopen = this.websocketopen
+      this.websock.onmessage = this.websocketonmessage
+      this.websock.onclose = this.websocketclose
+      this.websock.onerror = this.websocketerror
+    },
+    // 打开
+    websocketopen() {
+      console.log('WebSocket连接成功')
+    },
+    // 数据接收
+    websocketonmessage(e) {
+      console.log(e)
+      this.productinfos = JSON.parse(e.data)
+    },
+    // 关闭
+    websocketclose() {
+      console.log('WebSocket关闭')
+    },
+    // 失败
+    websocketerror() {
+      console.log('WebSocket连接失败')
+    }
   }
 }
 </script>
